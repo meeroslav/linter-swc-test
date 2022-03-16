ea2e7b2097bbc81e096da7699fe9763dba509272
Miroslav Jonas <missing.manual@gmail.com>
2022-03-16 12:50:18 +0100

    add windows check

--- .github/workflows/script.js
+++ .github/workflows/script.js
@@ -3,8 +3,5 @@ const { context } = require('@actions/github');
 process.stdout.write('\n');
 process.stdout.write(context.eventName);
 process.stdout.write('\n');
-process.stdout.write(JSON.stringify(context, null, 2));
-process.stdout.write('\n');
 process.stdout.write(JSON.stringify(context.repo, null, 2));
 process.stdout.write('\n');
-process.stdout.write(JSON.stringify(context.repository, null, 2));
